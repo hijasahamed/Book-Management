8f39ab7e03a8cf9ebffdcd6736f70501246ef567
hijas <hijasahamed2000@gmail.com>
2024-11-08 18:47:44 +0530

    login page obscure pasword completed

--- lib/src/presentation/widgets/common_widgets/custom_text_form_field.dart
+++ lib/src/presentation/widgets/common_widgets/custom_text_form_field.dart
@@ -1,7 +1,10 @@
+import 'package:book_management/src/presentation/bloc/login_screen_bloc/bloc/login_screen_bloc.dart';
 import 'package:book_management/src/presentation/widgets/common_widgets/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CustomTextFormField extends StatelessWidget {
+class CustomTextFormField extends StatefulWidget {
   final String hintText;
   final TextInputType keyboardType;
   final Size screenSize;
@@ -10,68 +13,91 @@ class CustomTextFormField extends StatelessWidget {
   final bool obscureText;
   final bool isPasswordForm;
 
-  const CustomTextFormField({
-    super.key,
-    required this.hintText,
-    required this.keyboardType,
-    required this.screenSize,
-    required this.controller,
-    required this.textCapitalization,
-    required this.obscureText,
-    required this.isPasswordForm
-  });
+  const CustomTextFormField(
+      {super.key,
+      required this.hintText,
+      required this.keyboardType,
+      required this.screenSize,
+      required this.controller,
+      required this.textCapitalization,
+      required this.obscureText,
+      required this.isPasswordForm});
 
   @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  bool obscureText = true;
+  @override
   Widget build(BuildContext context) {
+    LoginScreenBloc loginScreenBloc = LoginScreenBloc();
     final inputDecoration = InputDecoration(
       filled: true,
       fillColor: colorWhite,
       border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(screenSize.width * (9 / 360)),
+        borderRadius:
+            BorderRadius.circular(widget.screenSize.width * (9 / 360)),
         borderSide: const BorderSide(
-          color:  Color(0xFFE6E6E6),
+          color: Color(0xFFE6E6E6),
         ),
       ),
       focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(screenSize.width * (9 / 360)),
+        borderRadius:
+            BorderRadius.circular(widget.screenSize.width * (9 / 360)),
         borderSide: const BorderSide(
           color: Color(0XFFF56C04),
           width: 2,
         ),
       ),
       enabledBorder: OutlineInputBorder(
-        borderSide: const BorderSide(color: Color(0xFFE6E6E6)),
-        borderRadius: BorderRadius.all(Radius.circular(screenSize.width * (9 / 360)))
-      ),
+          borderSide: const BorderSide(color: Color(0xFFE6E6E6)),
+          borderRadius: BorderRadius.all(
+              Radius.circular(widget.screenSize.width * (9 / 360)))),
       contentPadding: EdgeInsets.symmetric(
-        vertical: screenSize.height / 60,
-        horizontal: screenSize.width / 25,
+        vertical: widget.screenSize.height / 60,
+        horizontal: widget.screenSize.width / 25,
       ),
-      hintText: hintText,
-      hintStyle: TextStyle(color: hintTextColor,fontSize: screenSize.width * (15 / 360),fontFamily: 'interRegular'),
-      suffixIcon:  isPasswordForm
-      ? IconButton(
-        onPressed: () {
-          
-        }, 
-        icon: Icon(obscureText? Icons.visibility : Icons.visibility_off)
-      )
-      : null    
+      hintText: widget.hintText,
+      hintStyle: TextStyle(
+          color: hintTextColor,
+          fontSize: widget.screenSize.width * (15 / 360),
+          fontFamily: 'interRegular'),
+      suffixIcon: widget.isPasswordForm
+          ? BlocBuilder<LoginScreenBloc, LoginScreenState>(
+              bloc: loginScreenBloc,
+              builder: (context, state) {
+                return IconButton(
+                  onPressed: () {
+                    obscureText = !obscureText;
+                    loginScreenBloc.add(PasswordObscureVariableSettingEvent());
+                  },
+                  icon: Icon(
+                      obscureText ? Icons.visibility : Icons.visibility_off),
+                );
+              },
+            )
+          : null,
     );
 
-    return TextFormField(
-      controller: controller,
-      decoration: inputDecoration,
-      textCapitalization: textCapitalization,
-      style: const TextStyle(color: Color(0XFF1D1D1F)),
-      obscureText: obscureText,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      keyboardType: keyboardType,
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return 'This field is required';
-        }
-        return null;
+    return BlocBuilder<LoginScreenBloc, LoginScreenState>(
+      bloc: loginScreenBloc,
+      builder: (context, state) {
+        return TextFormField(
+          controller: widget.controller,
+          decoration: inputDecoration,
+          textCapitalization: widget.textCapitalization,
+          style: const TextStyle(color: Color(0XFF1D1D1F)),
+          obscureText: widget.isPasswordForm ? obscureText : false,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          keyboardType: widget.keyboardType,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'This field is required';
+            }
+            return null;
+          },
+        );
       },
     );
   }
